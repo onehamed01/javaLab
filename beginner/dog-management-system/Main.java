b3cd8626e614937a189f343f806c5fbc0b783ab1
onehamed01 <hami.mdv01@Gmail.com>
2026-05-12 13:25:46 +0100

    Dog class test

--- beginner/dog-management-system/Main.java
+++ beginner/dog-management-system/Main.java
@@ -1,6 +1,23 @@
 class Main{
-    public static void main(String[] args){
-        System.out.println("Test");
-        
+    public static void main(String[] args) {
+        Dog wolfDog = new Dog("Wolf", 4);
+
+        wolfDog.bark();
     }
+}
+class Dog{
+    String name;
+    int age;
+
+    Dog(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    void bark(){
+        System.out.println(
+        "Hello my name is "+name+
+        ". I'm"+age+
+        " years old.");
+    }
+
 }
