9b519901dad9cdf11a41b1b1d57b4ee2531454cf
onehamed01 <hami.mdv01@Gmail.com>
2026-05-12 15:10:07 +0100

    Rewrite code to pracice. Main is code runner. Dog is a class of every objects

--- beginner/dog-management-system/Main.java
+++ beginner/dog-management-system/Main.java
@@ -1,9 +1,19 @@
 class Main{
-    public static void main(String[] args) {
-        Dog wolfDog = new Dog("Wolf", 4);
+    public static void main(String[] args){
+        Dog wolf = new Dog("Wolf", 3);
+        Dog mike = new Dog("Mike", 3);
+        Dog tyni = new Dog("Tyni", 3);
+        Dog mia = new Dog("Mia", 3);
+        Dog nef = new Dog("Nef", 3);
+        Dog testi = new Dog("testi", 3);
 
-        wolfDog.bark();
+        Dog[] dogList = {wolf, mike, tyni, mia, nef, testi};
+        
+        for (int idx = 0; idx < dogList.length; idx++){
+            dogList[idx].burk();
+        }
     }
+
 }
 class Dog{
     String name;
@@ -13,11 +23,9 @@ class Dog{
         this.name = name;
         this.age = age;
     }
-    void bark(){
+    void burk(){
         System.out.println(
-        "Hello my name is "+name+
-        ". I'm"+age+
-        " years old.");
+            "Hello, my name is "+name+" and I'm "+age+" years old."
+        );
     }
-
 }
