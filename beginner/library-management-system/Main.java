9fbdca10c838abff158cfaa1077f1d583746d291
onehamed01 <hami.mdv01@Gmail.com>
2026-05-19 23:32:32 +0100

    Add all methods again. showInfo to use methods overleading.

--- beginner/library-management-system/Main.java
+++ beginner/library-management-system/Main.java
@@ -1,31 +1,66 @@
-class LibraryItem {
-    String title;
-    boolean isBarrowed;
+class LibraryItem{
+    private String title;
+    private boolean borrowedStatus;
 
     LibraryItem(String title){
         this.title = title;
-        this.isBarrowed = false;
+        this.borrowedStatus = false;
     }
-    void barrowedItem(){
-        System.out.println("Barrowed Item");
+    void borrowItem(){
+
+        String title = this.title;
+
+        if (!this.borrowedStatus){
+            this.borrowedStatus = true;
+            System.out.println(title + " has been borrowed.");
+        }
+        else{
+            System.out.println(title + " is already borrowed.");
+        }
     }
     void returnItem(){
-        System.out.println("Returned Item");
+        if (this.borrowedStatus){
+            this.borrowedStatus = false;
+            System.out.println(title + " has been returned. Thank you.");
+        }
+        else{
+            System.out.println(title + " has already returned.");
+        }
     }
-
+    void showInfo(){
+        System.out.println("Books Name: "+ this.title);
+        if (!this.borrowedStatus){
+            System.out.println("Book is available");
+        }
+        else{
+            System.out.println("Book is Not available");
+        }
+    }   
 }
 
-class Books extends LibraryItem{
+class Book extends LibraryItem{
     String author;
 
-    Books(String author, String title){
+    Book (String author, String title){
         super(title);
         this.author = author;
     }
+    void showInfo(){
+        super.showInfo();
+        System.out.println("Author name: "+this.author);
+    }
+
 }
-public class Main{
+
+class Main{
     public static void main(String[] args){
-        LibraryItem atomicHabbit = new LibraryItem("Atomic Habbit");
-        String atomicHabbit.author = "James Clear";
+        Book atomichabbit = new Book("James clear", "Atomic Habbit.");
+        atomichabbit.showInfo();
+        atomichabbit.borrowItem();
+        atomichabbit.borrowItem();
+        atomichabbit.showInfo();
+        atomichabbit.returnItem();
+        atomichabbit.returnItem();
+
     }
 }
